8c2ebb3fcf830dbe5af1f0f0efb734724386f0c9
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 17:02:25 +0000

    91b668a07f24d3c8026d9ad8d2229def87eaf842 Release 1.5.0
    Sync to source repo @91b668a07f24d3c8026d9ad8d2229def87eaf842

--- js/select.bootstrap.mjs
+++ js/select.bootstrap.mjs
@@ -1,11 +1,10 @@
-
 /*! Bootstrap 3 styling wrapper for Select
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs';
-import DataTable from 'datatables.net-select';
+import 'datatables.net-select';
 
 
 
